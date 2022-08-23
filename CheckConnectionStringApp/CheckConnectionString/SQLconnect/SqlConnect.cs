bb5947edfccab76134f2059bb07237d7795c5cdb
baba564 <58572111+baba564@users.noreply.github.com>
2022-08-23 21:40:58 +0200

    program

---
+++ CheckConnectionStringApp/CheckConnectionString/SQLconnect/SqlConnect.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CheckConnectionString.SQLconnect
+{
+    public class SqlConnect
+    {
+        public static void CheckConnect(string connectionString)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    // Do work here; connection closed on following line.
+                    connection.Close();
+                    var connectionS= connection.ConnectionString;
+                    MessageBox.Show($"Zalogowano pomyślnie. Connection string: {connectionS}","Pomyślnie zalogowano",MessageBoxButton.OK,MessageBoxImage.Information);
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show($"Bład logowania: {e}","Error", MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ CheckConnectionStringApp/CheckConnectionString/ViewModel/MainViewModel.cs
@@ -0,0 +1,123 @@
+﻿using CheckConnectionString.Commands;
+using CheckConnectionString.SQLconnect;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CheckConnectionString.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _ShowConnectionString;
+        private string _Serwer { get; set; }
+        private string _DataBase { get; set; }
+        private string _User { get; set; }
+        private string _Password { get; set; }
+        public MainViewModel()
+        {
+            
+            ConnectionCommand = new RelayCommand(Connection);
+            ClearCommand = new RelayCommand(Clear);
+           
+        }
+        public void Connection(object obj)
+        {           
+            string connectionString = checkShowConnectionString();
+            if (ShowConnectionString.Trim().Length == 0)
+            {
+                if (Check == false)
+                {
+                    connectionString = $"Server={Serwer};Database={DataBase};User Id={User};Password={Password};";
+                }
+                else
+                {
+                    connectionString = $"Server={Serwer};Database={DataBase};Trusted_Connection=True;";
+                }
+            }
+
+
+            SqlConnect.CheckConnect(connectionString);
+            ShowConnectionString = connectionString;
+        }
+        public void Clear(object obj)
+        {
+            Serwer = "";
+            DataBase = "";
+            User = "";
+            Password = "";
+            ShowConnectionString = "";
+        }
+
+        
+        public string Serwer
+        {
+            get => _Serwer;
+            set
+            {
+                _Serwer = value;
+                OnPropertyChanged();
+            }
+        }
+        public string DataBase
+        {
+            get => _DataBase;
+            set
+            {
+                _DataBase = value;
+                OnPropertyChanged();
+            }
+        }
+        public string User
+        {
+            get => _User;
+            set
+            {
+                _User = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Password
+        {
+            get => _Password;
+            set
+            {
+                _Password = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool Check { get; set; }
+        public string ShowConnectionString
+        {
+            get => _ShowConnectionString;
+            set
+            {
+                _ShowConnectionString = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand ConnectionCommand { get; set; }
+        public ICommand ClearCommand { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string checkShowConnectionString()
+        {
+            if(ShowConnectionString==null)
+            {
+                return ShowConnectionString = "";
+            }
+            return ShowConnectionString;
+
+        }
+    }
+}
